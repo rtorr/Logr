7f9d182eecacec80012c4d9b3fae93a36ef45e9f
James Brewer <james@brewerhimself.com>
2012-07-12 23:08:22 -0400

    Scrapped old files and code that is no longer relevant.

--- logr.py
+++ logr.py
@@ -1,11 +1,10 @@
 from os import listdir
 from os.path import isdir
 
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-                  render_template, flash
+from flask import Flask, render_template
 from flaskext.markdown import Markdown
 
-from utils import establish_db_connection, slugify
+from utils import slugify
 
 logr = Flask(__name__)
 logr.config.from_object('config')
@@ -32,52 +31,8 @@ def index():
                 with open('articles/' + file_, 'r') as f_open:
                     title=f_open.readline()
                     files['Miscellaneous'].append(dict(file_=file_, slug=slugify(title), title=title))
-    print files
-    return render_template('index.html', files=files)
-
-@logr.route('/new', methods=['POST', 'GET'])
-def new():
-    """
-    Display the 'new post' form. If request type is POST, form has been
-    submitted and will be processed.
-    """
-    # If there is no session, the user does not belong here.
-    if not session.get('logged_in'):
-        abort(401)
-
-    if request.method == 'POST':
-        article_info = [request.form['post-title'], 
-                        request.form['post-body'],
-                        request.form['post-cat'],
-                        slugify(request.form['post-title'])]
-                        
-        g.db.execute('INSERT INTO articles (title, body, cat, slug) values \
-                     (?, ?, ?, ?)', article_info)
-        g.db.commit()
-        flash('"%s" was successfully posted.' % (request.form['post-title'],))
-        return redirect(url_for('index'))
     
-    categories = g.db.execute('SELECT name FROM categories ORDER BY name DESC')
-    categories = categories.fetchall()
-    return render_template('new.html', categories=categories)
-
-@logr.route('/cat', methods=['POST', 'GET'])
-def cat():
-    """
-    Display the 'new category' form. If request type is POST, form has been 
-    submitted and will be processed.
-    """
-    if not session.get('logged_in'):
-        abort(401)
-
-    if request.method == 'POST':
-        g.db.execute('INSERT INTO categories (name) values (?)',
-                     [request.form['cat-name']])
-        g.db.commit()
-        flash('Category "%s" has been added to the database.' %
-                (request.form['cat-name']))
-        return redirect(url_for('index'))
-    return render_template('cat.html')
+    return render_template('index.html', files=files)
 
 @logr.route('/b/<slug>', methods=['GET'])
 def show(slug):
@@ -97,52 +52,6 @@ def show(slug):
                     article = 'articles/' + dir_
 
     return render_template('show.html', article=open(article, 'r').read())
-
-@logr.route('/login', methods=['POST', 'GET'])
-def login():
-    """
-    Display the login form for the user. If the request type is 'POST' then we
-    validate the user's username and password combination. If they are correct,
-    then create a session and redirect them to the front page. Otherwise,
-    display and error.
-    """
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != logr.config['USERNAME']:
-            error = 'Invalid username.'
-        elif request.form['password'] != logr.config['PASSWORD']:
-            error = 'Invalid password.'
-        else:
-            session['logged_in'] = True
-            flash('Welcome back!')
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
-
-@logr.route('/logout')
-def logout():
-    """
-    Log the user out.
-    """
-    session.pop('logged_in', None)
-    flash('Goodbye!')
-    return redirect(url_for('index'))
-
-@logr.before_request
-def before_request():
-    """
-    A special function that get's called before each request and establishes a
-    connection to the database.
-    """
-    g.db = establish_db_connection(logr.config['DATABASE_FILE'])
-
-@logr.teardown_request
-def teardown_request(exception):
-    """
-    A special function that gets called after the response has been constructed
-    and closes the database connection opened by before_request().
-    """
-    g.db.close()
-
 
 if __name__ == '__main__':
     logr.run()
--- utils.py
+++ utils.py
@@ -1,41 +1,4 @@
 import re
-import sqlite3
-
-from contextlib import closing
-from flask import Flask
-from unidecode import unidecode
-
-
-def establish_db_connection(db):
-    """
-    Establish a connection to the database passed by the calling function.
-    """
-    return sqlite3.connect(db)
-
-def initialize_db():
-    """
-    Before the application can be run, the database must be constructed. Using
-    the sqlite3 <path> < <schemafile> command requires that the host have
-    sqlitee3 installed, which isn't always the case.
-
-    initialize_db() makes life easier by allowing the developer to initialize
-    the database in three simple steps:
-
-    1) $ python
-    2) >>> from utils import initialize_db
-    3) >>> initialize_db()
-    """
-    logr = Flask(__name__)
-    logr.config.from_object('config')
-
-    try:
-        with closing(establish_db_connection(logr.config['DATABASE_FILE'])) as db:
-            with logr.open_resource('schema.sql') as f:
-                db.cursor().executescript(f.read())
-            db.commit()
-        print "Database successfully initialized!"
-    except Error as e:
-        print "Error({0}): {1}".format(e.errno, e.strerror)
 
 def slugify(text):
     """
